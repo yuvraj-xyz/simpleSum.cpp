d64b62cbfbfb9568b87b4d8e81f21f430194c41e
yuvraj.cpp <78093228+yuvicpp@users.noreply.github.com>
2022-02-05 07:23:58 -0600

    Add files via upload

---
+++ simple sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 4;
+    int y = 5;
+    //int z = x - y;
+    cout << x+y;
+
+    return 0;
+
+}
---
+++ sum using cin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    int y;
+
+    cout << "Enter The First Digit x: ";
+    cin >> x;
+
+    cout << "Enter The Second Digit y: ";
+    cin >> y;
+
+    int sum = x + y;
+    cout << "Sum of The two Digits x and y is: " << sum;
+
+    return 0;
+}
